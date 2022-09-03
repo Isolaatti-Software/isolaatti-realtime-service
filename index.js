5c189f9ac762139c9f36341cea987b0665319a67
Erik Cavazos <erik10cavazos@gmail.com>
2022-09-03 13:59:58 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,88 @@
+const ISOLAATTI_BACKEND_SERVER = "http://localhost:5000";
+
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const bodyParser = require("express");
+const axios = require("axios").default;
+
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+        cors: {
+            origin: ["http://localhost:5000", "http://10.0.0.9:5000", "https://www.isolaatti.com"],
+            credentials: true
+        }
+    }
+);
+
+io.use((socket, next) => {
+    const token = socket.handshake.auth.sessionToken;
+    console.log(token);
+    // Make authentication against Isolaatti backend
+    axios.request({
+        url:`${ISOLAATTI_BACKEND_SERVER}/api/LogIn/Verify`,
+        method: "post",
+        headers: {
+            'Content-Type': 'application/json',
+            'sessionToken': token
+        }
+    }).then(response => {
+        const authResult = response.data;
+        if(authResult.isValid) {
+            socket.join(`user-${authResult.userId}`);
+            socket.on("subscribe-scope", (scopeData) => {
+                console.log(scopeData);
+                socket.join(`${scopeData.type}-${scopeData.id}`);
+            });
+            next();
+            console.log("Successful authentication");
+        } else {
+            next({message: "Authentication failed, invalid token", data: authResult});
+        }
+
+    });
+
+});
+
+async function validateRemoteKey(key){
+    const response = await axios.request({
+        url: `${ISOLAATTI_BACKEND_SERVER}/realtime-service/verify-key`,
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data: {
+            key: key
+        }
+    });
+
+    return response.status === 200;
+}
+
+app.post("/send_notification", async (req, res) => {
+   const payload = req.body;
+   if(!await validateRemoteKey(payload.secret)){
+       res.send({status: "invalid"});
+   }
+
+   const userId = payload.userId;
+   io.to(`user-${userId}`).emit("notification", payload.data);
+   res.send();
+});
+
+app.post("/update_event", async (req, res) => {
+    const payload = req.body;
+    if(!await validateRemoteKey(payload.secret)){
+        res.send({status: "invalid"});
+    }
+    const updateEventData = payload.eventData;
+    io.to(`${updateEventData.type}-${updateEventData.id}`).emit("post-update",updateEventData.data);
+    console.log(`evento emitido ${updateEventData.type}-${updateEventData.id}`, );
+    res.send();
+});
+
+httpServer.listen(3000);
